9767fe1ac0dc61a10847964e0556cbc6e14aebfa
liuyandut <liuyandut@163.com>
2015-07-29 09:40:43 +0800

    first commit

---
+++ src/main/java/com/liuyan/designPattern/Test.java
@@ -0,0 +1,10 @@
+package com.liuyan.designPattern;
+
+/**
+ * Created by liuyan.liu@qunar.com on 15-7-29.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
